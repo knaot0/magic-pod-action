97e542f4d6d4820ac3468c044b50d2a8e0cc1247
Naoto92X82V99 <naoto5018@gmail.com>
2022-03-05 10:34:59 +0900

    feat: add index.js

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
+const core = require("@actions/core");
+const { execSync } = require("child_process");
+
+const API_TOKEN = core.getInput("API_TOKEN");
+const ORGANIZATION = core.getInput("ORGANIZATION");
+const PROJECT = core.getInput("PROJECT");
+const TEST_SETTINGS_NUMBER = core.getInput("TEST_SETTINGS_NUMBER");
+
+const command = [
+  "./magicpod-api-client",
+  "batch-run",
+  `-t ${API_TOKEN}`,
+  `-o ${ORGANIZATION}`,
+  `-p ${PROJECT}`,
+  `-S ${TEST_SETTINGS_NUMBER}`,
+].join(" ");
 
 try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+  console.log(`wait until tests to be finished...`);
+  execSync(command);
 } catch (error) {
   core.setFailed(error.message);
 }
